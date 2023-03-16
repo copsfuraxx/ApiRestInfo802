1f9dc9ced897db6260e39fca7b09efacfa1676cd
copsfuraxx <55241502+copsfuraxx@users.noreply.github.com>
2023-03-16 08:08:13 +0100

    init():

---
+++ app.js
@@ -0,0 +1,27 @@
+import cors from 'cors'
+import express from 'express'
+
+var app = express();
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// Home route
+app.get("/api", (req, res) => {
+  res.json({ message: "Welcome to RouteCost API." });
+});
+
+// Un véhicule électrique consomme entre 12 et 20 ( soit 16 en moyenne ) kWh/100 km
+// Le prix au kilowattheure est d'environ 0,1582 € TTC.
+app.post('/api/cost', (req, res) => {
+    let distance = req.body.distance;
+    let cost = distance * 16 / 100 * 0.1582;
+    res.send({cost: cost});
+})
+
+app.listen(3000, () => { console.log("Server running on port 3000"); });
